e7e63fcada72ac99523cf51a07f4288dd137bada
divyam751 <mi.divyam@gmail.com>
2024-08-18 10:04:04 +0530

    hospital-management models

---
+++ 4-dataModeling/models/hospital-management/hospital.models.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const hospitalSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    addressLine1: { type: String, required: true },
+    addressLine2: { type: String },
+    city: { type: String, required: true },
+    pincode: { type: String, required: true }, //keep pincode in string , some countries have character in pincode
+    specializedIn: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Hospital = mongoose.model("Hospital", hospitalSchema);
